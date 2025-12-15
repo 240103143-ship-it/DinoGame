fc70ebfed13a0b0ae0f7ada238df399d0ef16f01
240103143-ship-it <240103143@sdu.edu.kz>
2025-12-15 09:27:51 +0500

    Update Obstacle (2).java

--- Obstacle (2).java
+++ Obstacle (2).java
@@ -1,34 +1,18 @@
-package game;
-
-import javafx.geometry.BoundingBox;
-import javafx.geometry.Bounds;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
-public class Obstacle {
-
-    public ImageView body;
-
-    public Obstacle(double x) {
-        Image img = new Image("file:src/game/cactus.png1.webp");
-        body = new ImageView(img);
-
-        body.setFitWidth(30);
-        body.setFitHeight(50);
-
-        body.setX(x);
-        body.setY(210);
-    }
-
-    // ---------- CUSTOM HITBOX for cactus ----------
-    public Bounds getHitBox() {
-        Bounds b = body.getBoundsInParent();
-
-        return new BoundingBox(
-                b.getMinX() + 5,       // сол жағынан азайту
-                b.getMinY() + 5,       // үстінен
-                b.getWidth() - 10,     // енін азайту
-                b.getHeight() - 10     // биігін азайту
+private void showGameOver() {
+        Label over = new Label("GAME OVER\n\nPRESS R TO RESTART");
+        over.setStyle(
+                "-fx-font-family: 'Press Start 2P';" +
+                        "-fx-font-size: 18px;" +
+                        "-fx-text-fill: black;" +
+                        "-fx-alignment: center;"
         );
+
+        over.setPrefWidth(WIDTH);
+        over.setLayoutY(HEIGHT / 2 - 40);
+        over.setAlignment(javafx.geometry.Pos.CENTER);
+
+        gamePane.getChildren().add(over);
+        over.toFront();
     }
 }
+\
