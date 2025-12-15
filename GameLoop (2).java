2f479ab5f269bf9dda0b7e1e3fa5eb30b6f51a6e
240103143-ship-it <240103143@sdu.edu.kz>
2025-12-15 09:29:23 +0500

    Update GameLoop (2).java

--- GameLoop (2).java
+++ GameLoop (2).java
@@ -141,3 +141,23 @@ public class GameLoop extends AnimationTimer {
             showGameOver();
         }
     }
+
+    private void showGameOver() {
+        Label over = new Label("GAME OVER\n\nPRESS R TO RESTART");
+        over.setStyle(
+                "-fx-font-family: 'Press Start 2P';" +
+                        "-fx-font-size: 18px;" +
+                        "-fx-text-fill: black;" +
+                        "-fx-alignment: center;"
+        );
+
+        over.setPrefWidth(WIDTH);
+        over.setLayoutY(HEIGHT / 2 - 40);
+        over.setAlignment(javafx.geometry.Pos.CENTER);
+
+        gamePane.getChildren().add(over);
+        over.toFront();
+    }
+}
+
+
